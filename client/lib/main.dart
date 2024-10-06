2f5b873e4d9415df28d4a117bc0787cabdc18fa0
Benjamin Ortiz <A01277673@tec.mx>
2024-10-06 11:08:25 -0600

    Add: Send JSON for Login

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert'; // For converting data to JSON format
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -18,6 +20,49 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginScreen extends StatelessWidget {
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void sendJsonData(context) async {
+    //Replace the # symbols with the actual numbers of your IP address
+    final url = Uri.parse('http://#.#.#.#:3000/users/login');
+
+    // Get the text from the TextField using the controller
+    String username = usernameController.text;
+    String password = passwordController.text;
+
+    // Create the JSON data
+    Map<String, dynamic> jsonData = {
+      'username': username,
+      'password': password,
+    };
+
+    // Send the POST request with the JSON body
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(jsonData),
+    );
+
+    if (response.statusCode == 200) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SecondScreen()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Invalid Credentials!. Please check the mail and password and try again.'),
+          backgroundColor: Colors.red,  // You can style it to look like a warning
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,20 +73,18 @@ class LoginScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
+              controller: usernameController,
               decoration: InputDecoration(hintText: 'Email'),
             ),
             TextField(
               obscureText: true,
+              controller: passwordController,
               decoration: InputDecoration(hintText: 'Password'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Navigate to Second Screen
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondScreen()),
-                );
+                sendJsonData(context);
               },
               child: Text('Login'),
             ),
