768f23c194c32274ddac135332e975bdf40d4812
Benjamin Ortiz <A01277673@tec.mx>
2024-10-07 12:38:08 -0600

    Update: added regular expressions to TextInputs

--- client/lib/screens/login_screen.dart
+++ client/lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert'; // For converting data to JSON format
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'home_screen_user.dart';
 
@@ -65,11 +66,15 @@ class LoginScreen extends StatelessWidget {
             TextField(
               controller: usernameController,
               decoration: InputDecoration(hintText: 'Email'),
+              //Regular expressions for preventing SQL Injections
+              inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[a-zA-Z0-9_.@]'))],
             ),
             TextField(
               obscureText: true,
               controller: passwordController,
               decoration: InputDecoration(hintText: 'Password'),
+              //Regular expressions for preventing SQL Injections
+              inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[a-zA-Z0-9_.]'))],
             ),
             SizedBox(height: 20),
             ElevatedButton(
