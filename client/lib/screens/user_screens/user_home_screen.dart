fe4d0fde0c8c61320d021e415c0309db549028b6
Alberto2708 <mentadoreyescarlosalberto@gmail.com>
2024-10-28 14:48:36 -0600

    Update: home_user_screens

--- client/lib/screens/user_screens/user_home_screen.dart
+++ client/lib/screens/user_screens/user_home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import '../user_profile.dart';
 
 class LocationCenter {
   final int id;
@@ -22,7 +23,7 @@ class LocationCenter {
 class UserHomeScreen extends StatefulWidget {
   final int userId;
 
-  UserHomeScreen({required this.userId});
+  UserHomeScreen({required this.userId, super.key});
 
   @override
   _UserHomeScreenState createState() => _UserHomeScreenState();
@@ -57,102 +58,130 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
     }
   }
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Column(
-      children: [
-        Expanded(
-          child: Container(),
-        ),
-        
-        const Padding(
-          padding: EdgeInsets.only(top: 20),
-          child: Text(
-            "Centros de acopio cerca de ti:",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(),
           ),
-        ),
-        
-        Expanded(
-          flex: 8,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10.0, top: 8.0, right: 10),
-            child: centers.isNotEmpty
-                ? GridView.builder(
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 20.0, 
-                      crossAxisSpacing: 20.0, 
-                      childAspectRatio: 1.2,
-                    ),
-                    itemCount: centers.length,
-                    itemBuilder: (context, index) {
-                      final center = centers[index];
-                      return Container(
-                        padding: const EdgeInsets.all(10.0), // Internal padding for each container
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 5,
-                              spreadRadius: 2,
-                              offset: Offset(0, 5),
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: double.infinity,
-                              height: 50,
-                              decoration: const BoxDecoration(color: Colors.grey),
-                              alignment: Alignment.center,
-                              child: const Text("[IMG]", style: TextStyle(color: Colors.white)),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Text(
-                              center.centerName,
-                              style: const TextStyle(fontSize: 14),
-                              textAlign: TextAlign.left,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
-                      );
+
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              "Centros de acopio cerca de ti:",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          Expanded(
+            flex: 8,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0, top: 8.0, right: 10),
+              child: centers.isNotEmpty
+                  ? GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 20.0,
+                        crossAxisSpacing: 20.0,
+                        childAspectRatio: 0.95, // Adjusts container height
+                      ),
+                      itemCount: centers.length,
+                      itemBuilder: (context, index) {
+                        final center = centers[index];
+                        return Container(
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20), 
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 5,
+                                spreadRadius: 2,
+                                offset: Offset(0, 5),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[400],
+                                  borderRadius: BorderRadius.circular(8), // Rounded image placeholder
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text("[IMG]", style: TextStyle(color: Colors.white)),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      center.centerName,
+                                      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Spacer(),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: FloatingActionButton(
+                                  onPressed: () {},
+                                  mini: true,
+                                  backgroundColor: Colors.red,
+                                  foregroundColor: Colors.white,
+                                  child: const Icon(Icons.add, size: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    )
+                  : const Center(child: CircularProgressIndicator()),
+            ),
+          ),
+
+          Expanded(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.home_outlined),
+                    color: const Color(0xFF000000),
+                    iconSize: 40.0,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => UserProfileScreen(userId: widget.userId)
+                        ));
                     },
-                  )
-                : const Center(child: CircularProgressIndicator()),
+                    icon: const Icon(Icons.person_2_outlined),
+                    color: const Color(0xFF000000),
+                    iconSize: 40.0,
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-
-        Expanded(
-          flex: 1,
-          child: Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: (){}
-                , child: Text("Home")
-                ),
-                ElevatedButton(
-                  onPressed: (){}
-                , child: Text("Profile")
-                )
-      ] 
-      )
-
-            ,)
-          )
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
 }
