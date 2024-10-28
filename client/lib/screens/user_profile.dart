b9908faa60b9ae5ccfd3947438a8025bcee382e7
Juan Avalos <111295746+juanvaloss@users.noreply.github.com>
2024-10-28 12:03:00 -0600

    Update Profile page

--- client/lib/screens/user_profile.dart
+++ client/lib/screens/user_profile.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class UserProfileScreen extends StatefulWidget{
+class UserProfileScreen extends StatefulWidget {
   final int userId;
 
   const UserProfileScreen({super.key, required this.userId});
@@ -28,22 +28,19 @@ class _UserInfoScreenState extends State<UserProfileScreen> {
   }
 
   String _generateMapUrl(double latitude, double longitude) {
-    const apiKey = '#'; // Replace with your API key
+    const apiKey = 'AIzaSyAEW6obJehSR'; // Replace with your API key
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=17&size=650x300&markers=color:red%7C$latitude,$longitude&key=$apiKey';
   }
 
   Future<void> fetchUserInfo() async {
     try {
-      // Replace the # with ur actual ip
-      final url1 = Uri.parse('http://#.#.#.#:3000/users/userInfo');
-      final url2 = Uri.parse('http://#.#.#.#:3000/users/userCenters');
+      final url1 = Uri.parse('http://192.168.100.9:3000/users/userInfo');
+      final url2 = Uri.parse('http://192.168.100.9:3000/users/userCenters');
 
-      // Prepare the JSON data
       Map<String, dynamic> jsonData = {
         'userId': widget.userId,
       };
 
-      // Send the first POST request
       final response1 = await http.post(
         url1,
         headers: {'Content-Type': 'application/json'},
@@ -56,7 +53,6 @@ class _UserInfoScreenState extends State<UserProfileScreen> {
       );
 
       if (response1.statusCode == 200 && response1.body.isNotEmpty) {
-
         final responseData1 = json.decode(response1.body);
         setState(() {
           userInfo = {
@@ -69,14 +65,23 @@ class _UserInfoScreenState extends State<UserProfileScreen> {
         });
       }
 
-      if(response2.statusCode == 200 && response2.body.isNotEmpty){
-        final Map<String, dynamic> responseData2 = json.decode(response2.body);
+      if (response2.statusCode == 200 && response2.body.isNotEmpty) {
+        final dynamic responseData2 = json.decode(response2.body);
 
-        setState(() {
-          // Merge the data from both responses if necessary
-          centerInfo = [responseData2];
-        });
-      }else{
+        if (responseData2 is List) {
+          setState(() {
+            centerInfo = List<Map<String, dynamic>>.from(responseData2);
+          });
+        } else if (responseData2 is Map<String, dynamic>) {
+          setState(() {
+            centerInfo = [responseData2];
+          });
+        } else {
+          setState(() {
+            errorMessage = 'Unexpected response format.';
+          });
+        }
+      } else {
         setState(() {
           errorMessage = 'No information available.';
         });
@@ -88,73 +93,146 @@ class _UserInfoScreenState extends State<UserProfileScreen> {
     }
   }
 
+  Widget _buildUserInfoSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.red[200],
+          child: Icon(Icons.person, size: 40, color: Colors.white),
+        ),
+        const SizedBox(height: 16),
+        Text(
+          userInfo?['name'] ?? 'Local Shop',
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 16),
+
+      ],
+    );
+  }
+
+  Widget _buildUserInfoContainer() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Color(0xFFF0F5FA),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'User Info:',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          ...userInfo!.entries
+              .where((entry) => entry.key.toLowerCase() != 'id') // Filtrar "Id"
+              .map((entry) {
+            return ListTile(
+              title: Text(_capitalizeFirstLetter(entry.key)),
+              subtitle: Text(entry.value.toString()),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildCenterInfoContainer() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Color(0xFFF0F5FA),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Center Info:',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          ...centerInfo!.map((center) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Name: ${center['name']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  "Address: ${center['address']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 8),
+                Center(
+                  child: Image.network(
+                    _generateMapUrl(
+                      center['latitude'],
+                      center['longitude'],
+                    ),
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('User Info')),
+      appBar: AppBar(title: const Text('User Profile')),
       body: userInfo != null && centerInfo != null
           ? Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            // Separator for userInfo
-            const Text(
-              'User Information:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            _buildUserInfoSection(),
+            _buildUserInfoContainer(),
+            _buildCenterInfoContainer(),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFEF3030),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                onPressed: () {
+                },
+                child: const Text(
+                  'Solicitud de Centro',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-            const SizedBox(height: 8),
-            ...userInfo!.entries.map((entry) {
-              return ListTile(
-                title: Text(_capitalizeFirstLetter(entry.key)),
-                subtitle: Text(entry.value.toString()),
-              );
-            }),
-
-            // Separator for centerInfo
-            const SizedBox(height: 16),
-            const Text(
-              'Center Information:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
-
-            // Display each center's information
-            ...centerInfo!.map((center) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Name: ${center['name']}",
-                    style: const TextStyle(fontSize: 16), // Set the font size for uniformity
-                  ),
-                  const SizedBox(height: 4), // Space between name and address
-                  Text(
-                    "Address: ${center['address']}",
-                    style: const TextStyle(fontSize: 16), // Same font size as the name
-                  ),
-                  const SizedBox(height: 8),
-                  Center(
-                    child: Image.network(
-                      _generateMapUrl(
-                        center['latitude'],
-                        center['longitude'],
-                      ),
-                      height: 200,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                ],
-              );
-            }),
           ],
         ),
       )
