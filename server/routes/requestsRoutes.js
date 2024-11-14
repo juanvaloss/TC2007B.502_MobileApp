4084caa68d2bdeef9fcfd54d8e8a8aac2ac02040
Benjamin Ortiz <elociodebenjamin@hotmail.com>
2024-11-14 13:15:48 -0600

    Update: requests logic.

--- server/routes/requestsRoutes.js
+++ server/routes/requestsRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const requestController = require('../controllers/requestsControllers');
 
+router.post('/getReqInfo', requestController.getRequestInfo);
 router.post('/create', requestController.createRequest);
 router.post('/delete', requestController.deleteRequest);
 
