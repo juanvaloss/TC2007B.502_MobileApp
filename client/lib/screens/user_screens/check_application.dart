1df39500216576a5a4c25d8c93db56d74e2bb8de
Alberto2708 <mentadoreyescarlosalberto@gmail.com>
2024-11-14 14:48:48 -0600

    UPDATE: check_application is done

--- client/lib/screens/user_screens/check_application.dart
+++ client/lib/screens/user_screens/check_application.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class CheckApplication extends StatefulWidget {
   final int userId;
 
@@ -14,8 +13,7 @@ class CheckApplication extends StatefulWidget {
 
 class _CheckApplicationState extends State<CheckApplication> {
   Map<String, dynamic>? applicationInfo;
-  String errormessage = '';
-
+  String errorMessage = '';
 
   @override
   void initState() {
@@ -23,133 +21,345 @@ class _CheckApplicationState extends State<CheckApplication> {
     checkApplicationInfo();
   }
 
-  String status = "Pendiente";
-
+  // Function to fetch application information
   Future<void> checkApplicationInfo() async {
     try {
-      final url = Uri.parse('http:/');
-    }
-    catch (e){
+      final url = Uri.parse('http://10.43.44.202:3000/requests/getReqInfo');
+      Map<String, dynamic> jsonData = {'userId': widget.userId.toString()};
 
-      print('Error; $e');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(jsonData),
+      );
 
+      if (response.statusCode == 200 && response.body.isNotEmpty) {
+        final responseData = json.decode(response.body);
+
+        if (responseData is List && responseData.isNotEmpty) {
+          setState(() {
+            applicationInfo = responseData[0];
+          });
+        } else {
+          setState(() {
+            errorMessage = 'No se encontró información del centro.';
+          });
+        }
+      } else {
+        setState(() {
+          errorMessage =
+              'Error ${response.statusCode}: no se pudo cargar la información del centro.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        errorMessage = 'Error: $e - No se pudo cargar la información del centro.';
+      });
     }
   }
 
-  List<Widget> getCompletedCourses(){
+  // Function to get icon based on completion status
+  Icon getIcon(bool? completed) {
+    return Icon(
+      completed == true ? Icons.check : Icons.close,
+      color: const Color(0xFF000000),
+    );
+  }
+
+  // Function to get container color based on completion status
+  Color getContainerColor(bool? completed) {
+    return completed == true ? const Color(0xFF70DA67) : const Color(0xFFECF0F4);
+  }
+
+  // Function to build the list of completed courses
+  List<Widget> getCompletedCourses() {
     List<Widget> items = [];
 
+    if (applicationInfo?['acceptsMeat'] == true) {
+      items.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: 10),
+            // Circle container with an icon
+            Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(applicationInfo?['completedCourse1']),
+                shape: BoxShape.circle,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: getIcon(applicationInfo?['completedCourse1']),
+            ),
+            // Elevated button acting as a course item
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Botón de preservación de carnes presionado");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF0F5FA), // Button background color
+                  foregroundColor: const Color(0xFF32343E), // Text color on press
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Border radius
+                  ),
+                ),
+                child: const Text(
+                  "Preservación de carnes",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+          ],
+        ),
+      );
+    }
+
+    items.add(const SizedBox(height: 15));
+
+    if(applicationInfo?['acceptsVegetables'] == true){
+      items.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: 10),
+            // Circle container with an icon
+            Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(applicationInfo?['completedCourse2']),
+                shape: BoxShape.circle,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: getIcon(applicationInfo?['completedCourse2']),
+            ),
+            // Elevated button acting as a course item
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Botón de preservación de vegetales presionado");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF0F5FA), // Button background color
+                  foregroundColor: const Color(0xFF32343E), // Text color on press
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Border radius
+                  ),
+                ),
+                child: const Text(
+                  "Preservación de vegetales",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+          ],
+        ),
+      );
+    }
+
+    items.add(const SizedBox(height: 15));
+
+    if(applicationInfo?['acceptsCans'] == true){
+      items.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: 10),
+            // Circle container with an icon
+            Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(applicationInfo?['completedCourse3']),
+                shape: BoxShape.circle,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: getIcon(applicationInfo?['completedCourse3']),
+            ),
+            // Elevated button acting as a course item
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Botón de preservación de latas presionado");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF0F5FA), // Button background color
+                  foregroundColor: const Color(0xFF32343E), // Text color on press
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Border radius
+                  ),
+                ),
+                child: const Text(
+                  "Preservación de enlatados",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+          ],
+        ),
+      );
+    }
+
+    items.add(const SizedBox(height: 15));
+
     items.add(
-      const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(Icons.check, color: Colors.green,)
-      ],)
-    
+      Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: 10),
+            // Circle container with an icon
+            Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(applicationInfo?['completedCourse4']),
+                shape: BoxShape.circle,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: getIcon(applicationInfo?['completedCourse4']),
+            ),
+            // Elevated button acting as a course item
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Organización de un centro de acopio presionado");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF0F5FA), // Button background color
+                  foregroundColor: const Color(0xFF32343E), // Text color on press
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Border radius
+                  ),
+                ),
+                child: const Text(
+                  "Organización de un centro de acopio",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18,),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+          ],
+        ),
     );
 
+    items.add(const SizedBox(height: 15));
 
+    items.add(
+      Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: 10),
+            // Circle container with an icon
+            Container(
+              decoration: BoxDecoration(
+                color: getContainerColor(applicationInfo?['completedCourse5']),
+                shape: BoxShape.circle,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: getIcon(applicationInfo?['completedCourse5']),
+            ),
+            // Elevated button acting as a course item
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Equipo necesario presionado");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF0F5FA), // Button background color
+                  foregroundColor: const Color(0xFF32343E), // Text color on press
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // Border radius
+                  ),
+                ),
+                child: const Text(
+                  "Equipo necesario",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18,),
+                ),
+              ),
+            ),
+            
+          const SizedBox(width: 10)
+          ],
+        ),
+    );
 
     return items;
-    }
+  }
 
-    Text getText(){
-      if(status == "Aprobado"){
-        return const Text(
-          "Aprobada",
-          style:TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20
-          ));
-      }
-      else if(status == "Pendiente"){
-        return const Text(
-          "Pendiente",
-          style:TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20
-          ));
-      }
-      else{
-        return const Text(
-          "Rechazada",
-          style:TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20
-          ));
-      }
-    }
-
-    Color getStatusColor(){
-      if(status == "Aprobado"){
-        return const Color(0xFF70DA67);
-      }
-      else if(status == "Pendiente"){
-        return const Color(0xFFFF9500);
-      }
-      else{
-        return const Color(0xFFEF3030);
-      }
-    }
-
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-    appBar: AppBar(
-      title: const Text('Estatus de la solicitud'),
-      backgroundColor: Colors.white,
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.black),
-        onPressed: () => Navigator.pop(context),
+      appBar: AppBar(
+        title: const Text('Estatus de la solicitud'),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        elevation: 0,
       ),
-      elevation: 0,
-    ),
-      body:  Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-            child: Column(children: [
-              const SizedBox(height: 100),
-
-              const Text(
-                'Capacitaciones para tu centro de acopio',
-                style: TextStyle( 
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-
-                )),
-
-              const SizedBox(height: 20),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: getCompletedCourses(),
+            child: Column(
+              children: [
+                const SizedBox(height: 100),
+                const Text(
+                  'Cursos para tu centro de acopio',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
-
-              const Text("Estatus de tu solicitud: "),
-
-              const SizedBox(height: 15),
-
-              Container(
-                width: 400,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: getStatusColor(),
-                  borderRadius: BorderRadius.circular(10),
+                const SizedBox(height: 50),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: getCompletedCourses(),
                 ),
-                child: Center(child: getText(),)
-              )
-
-
-            ],)
-          )
+                const SizedBox(height: 100),
+                const Text("Estatus de tu solicitud: "),
+                const SizedBox(height: 15),
+                Container(
+                  width: 400,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFF9500),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Pendiente",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
